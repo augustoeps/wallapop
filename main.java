33d23d9536b1ef76fb1a116f719a7993acae7977
augusto <augustopaolone3@hotmail.com>
2022-12-21 16:19:24 +0100

    update

---
+++ main.java
@@ -0,0 +1,19 @@
+
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+
+
+        
+        System.out.println("ggg");
+        
+
+       
+
+    }
+    
+}
